f699ed7002c1725988f4e69e1efc4ce20ca1b19a
Drew Griffin <dgriffin394@gmail.com>
2025-07-08 20:35:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,6 @@ if herb:
 st.sidebar.subheader("Recent Herbs Searched")
 for past in reversed(st.session.state.history[-5:]):
     st.sidebar.write(past)
-    
 except Exception as e:
     st.error("Something went wrong.")
     st.code(str(e))
